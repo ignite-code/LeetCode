2b0c4add5feab054df9dd7378cb3e27c63859cd1
Shubhangi <57293871+ignite-code@users.noreply.github.com>
2025-07-08 20:12:00 +0530

    Time: 3 ms (96.64%), Space: 58.3 MB (14.13%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,33 +1,33 @@
 class Solution {
     public int maxArea(int[] height) {
-        // int start=0;
-        // int end=height.length-1;
-        // int max=0;
-        // while(start<end){
-        //     if(height[start]<height[end]){
-        //         max = Math.max(max,height[start]*(end-start));
-        //         start++;
-        //     }
-        //     else{
-        //         max = Math.max(max,height[end]*(end-start));
-        //         end--;
-        //     }
-        // }
-        // return max;
-
-        int i=0,j=height.length-1;
-        int maxWater = 0;
-        while(j-i!=0){
-            int max=Math.min(height[i],height[j])*(j-i);
-            if(height[i]<height[j]){
-                i++;
-            }else{
-                j--;
-            } 
-            if(max>maxWater){
-                maxWater=max;
-            }  
+        int start=0;
+        int end=height.length-1;
+        int max=0;
+        while(start<end){
+            if(height[start]<height[end]){
+                max = Math.max(max,height[start]*(end-start));
+                start++;
+            }
+            else{
+                max = Math.max(max,height[end]*(end-start));
+                end--;
+            }
         }
-        return maxWater;
+        return max;
+
+        // int i=0,j=height.length-1;
+        // int maxWater = 0;
+        // while(j-i!=0){
+        //     int max=Math.min(height[i],height[j])*(j-i);
+        //     if(height[i]<height[j]){
+        //         i++;
+        //     }else{
+        //         j--;
+        //     } 
+        //     if(max>maxWater){
+        //         maxWater=max;
+        //     }  
+        // }
+        // return maxWater;
     }
 }
