b30c317dde8206d69c18d72292cc193e80814c82
Shubhangi <57293871+ignite-code@users.noreply.github.com>
2026-05-04 11:41:09 +0530

    Time: 4 ms (94.39%), Space: 77 MB (91.94%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,33 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
-        int start=0;
-        int end=height.length-1;
-        int max=0;
-        while(start<end){
-            if(height[start]<height[end]){
-                max = Math.max(max,height[start]*(end-start));
-                start++;
+        if(height.length == 0 || height.length == 1){
+            return -1;
+        }
+        int left = 0;
+        int right = height.length-1;
+        int max = 0;
+        while(left != right){
+            
+            if(height[left] < height[right]){
+                max = Math.max(max,height[left]*(right-left));
+                left++;
             }
             else{
-                max = Math.max(max,height[end]*(end-start));
-                end--;
+                max = Math.max(max,height[right]*(right-left));
+                right--;
             }
         }
         return max;
-
-        // int i=0,j=height.length-1;
-        // int maxWater = 0;
-        // while(j-i!=0){
-        //     int max=Math.min(height[i],height[j])*(j-i);
-        //     if(height[i]<height[j]){
-        //         i++;
-        //     }else{
-        //         j--;
-        //     } 
-        //     if(max>maxWater){
-        //         maxWater=max;
-        //     }  
-        // }
-        // return maxWater;
     }
 }
