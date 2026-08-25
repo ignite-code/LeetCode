e33b4d8b1d249bcadecebe247c432b07b451bd66
Shubhangi <57293871+ignite-code@users.noreply.github.com>
2026-08-26 00:16:31 +0530

    Time: 0 ms (100%), Space: 47.4 MB (33.04%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,13 +1,21 @@
 import java.util.*;
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == i) continue;
-            else{
-                return i;
-            }
+        int len = nums.length;
+        int expectedTotal = len * (len+1) / 2;
+        int actualTotal=0;
+        for(int i=0;i<len;i++){
+            actualTotal += nums[i];
         }
-        return nums.length;
+        return expectedTotal-actualTotal;
+
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i] == i) continue;
+        //     else{
+        //         return i;
+        //     }
+        // }
+        // return nums.length;
     }
 }
